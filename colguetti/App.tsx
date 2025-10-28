b280661afbdbcb7252a9c286d56140e3aa736d0a
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-10-28 09:19:50 -0300

    unused import

--- colguetti/App.tsx
+++ colguetti/App.tsx
@@ -1,4 +1,3 @@
-import { View } from 'react-native';
 import Navigator from './components/screens/Navigator';
 import { ConfigProvider } from './contexts/config-context';
 import { ThemeProvider } from './contexts/theme-context';
