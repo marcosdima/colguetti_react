c42aba7baa9ae65a32b6845e3469be84e91091a5
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-10-28 10:57:19 -0300

    style rework - own components implementation

--- colguetti/components/screens/Home.tsx
+++ colguetti/components/screens/Home.tsx
@@ -1,10 +1,11 @@
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { View, Button } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { RootStackParamList } from "../../types/root-stack";
 import { useConfig } from "../../contexts/config-context";
 import Title from "../base/Title";
 import SubTitle from "../base/SubTitle";
+import Button from "../inputs/Button";
 
 type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Home'>;
 
@@ -13,9 +14,8 @@ export default function Home() {
   const { config } = useConfig();
   
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={styles.container}>
       <Title>Main Screen</Title>
-      <Button title="Go to config" onPress={() => navigation.navigate('Configuration')} />
       <SubTitle>
         {
           config?.alias
@@ -23,6 +23,16 @@ export default function Home() {
             : 'Hi there!'
         }
       </SubTitle>
+      <Button text="Go to config" onPress={() => navigation.navigate('Configuration')} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingTop: 40,
+  },
+});
